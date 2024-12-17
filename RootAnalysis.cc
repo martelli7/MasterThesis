f068f66d06eb68bd423d683f016482f2147b65f9
Mattia Martelli <m.martelli7@campus.unimib.it>
2024-12-17 22:32:46 +0100

    Update RootAnalysis.cc
    Funzionante

--- RootAnalysis.cc
+++ RootAnalysis.cc
@@ -1,8 +1,8 @@
-// $Id: RootAnalysis.cc 2 2024-12-12 martelli $
+// $Id: RootAnalysis.cc 3 2024-12-17 martelli $
 /**
  * @file   RootAnalysis.cc
  *
- * @date   12 Dec 2024
+ * @date   17 Dec 2024
  * @author martelli
  *
  * @brief  Simple program for opening a previous .root file
@@ -56,120 +56,151 @@
 
 int main()
 {
-    ROOT::EnableImplicitMT(); // multi-threading
-
-    auto fileName = "Tesi_NN_0.root";
-    auto treeName = "Hits;-32768";
-
-    // apriamo il RootDataFrame
-    ROOT::RDataFrame df(treeName, fileName);
-
-
-
-    // Filtering
-    auto df_filtered = df.Filter("nCrystalCompton == 1", "Events with just 1 Compton Scattering")
-    .Filter("parentID==0","Photons which are not daughters") //domanda: gli eventi con parentID = 2,3,4 cosa sono...?
-    .Filter("posX!=0 && posY!=0 && posZ!=0", "Events with photons")
-    .Filter("photonID==1","Select the first photon only")
-    //.Filter()   //possibile concatenare più filtri
-    //.Count();   //Count() non funziona usando anche Snapshot()
-    .Snapshot("filteredHits", "filtered_file_photon1.root", {"posX", "posY", "posZ", "photonID", "eventID", "parentID", "nCrystalCompton"});
-    //il metodo Snapshot() crea un nuovo file con i filtri e le colonne selezionate, ora il file è molto leggero
-
-    //controlla quante entrate hanno superato il filto
-    //std::cout << *df_filtered << " entries passed all filters" << std::endl;
-
-    // Histo2D posX:posY -> funziona!
-    /*auto myHist1 = df.Histo2D({"histName", "posX:posY{nCrystalCompton==1}", 64u, -150., 150., 32u, -150., 150.}, "posX", "posY");
+    /*
+     *    ROOT::EnableImplicitMT(); // multi-threading
+     *
+     *    auto fileName = "Tesi_NN_0.root";
+     *    auto treeName = "Hits;-32768";
+     *
+     *    // apriamo il RootDataFrame
+     *    ROOT::RDataFrame df(treeName, fileName);
+     *
+     *
+     *
+     *    // Filtering
+     *    auto df_filtered = df.Filter("nCrystalCompton == 1", "Events with just 1 Compton Scattering")
+     *    .Filter("parentID==0","Photons which are not daughters") //domanda: gli eventi con parentID = 2,3,4 cosa sono...?
+     *    .Filter("posX!=0 && posY!=0 && posZ!=0", "Events with photons")
+     *    //.Filter("photonID==1","Select the first photon only")
+     *    //.Filter()   //possibile concatenare più filtri
+     *    //.Count();   //Count() non funziona usando anche Snapshot()
+     *    .Snapshot("filteredHits", "filtered_file3.root", {"posX", "posY", "posZ", "photonID", "eventID", "parentID", "nCrystalCompton"});
+     *    //il metodo Snapshot() crea un nuovo file con i filtri e le colonne selezionate, ora il file è molto leggero
+     *
+     *    //controlla quante entrate hanno superato il filto
+     *    //std::cout << *df_filtered << " entries passed all filters" << std::endl;
+     *
+     *    // Histo2D posX:posY -> funziona!
+     *    /*auto myHist1 = df.Histo2D({"histName", "posX:posY{nCrystalCompton==1}", 64u, -150., 150., 32u, -150., 150.}, "posX", "posY");
      *
      *    TCanvas c;
      *    myHist1->Draw("COLZ"); //scala a colori
      *    c.SaveAs("posXposY.root");
      */
 
-    //----------------------------------------------------------//
-    // NOTA: questa parte non funziona, ma secondo me l'idea è utilizzabile
-    // mi dà un errore di memoria a riga 113 e 124 quando cerco di accedere agli array... forse il problema è risolvibile semplicemente dichiarandoli di grandezza nota e fine
-    // ora apriamo il nuovo file root e analizziamo le posizioni
+     //----------------------------------------------------------//
+
+     std::unique_ptr<TFile> myFile( TFile::Open("filtered_file3.root") );
+     auto tree = myFile->Get<TTree>("filteredHits");
+
+     Long64_t nEntries = tree->GetEntries(); //number of entries inside each Branch (=9677397)
+
+     // Branches
+     Float_t posX;
+     Float_t posY;
+     Float_t posZ;
+     Int_t photonID;
+     Int_t eventID;
+
+     // My arrays
+     Float_t *myposX = new Float_t[nEntries];
+     Float_t *myposY = new Float_t[nEntries];
+     Float_t *myposZ = new Float_t[nEntries];
+     Int_t *myphotonID = new Int_t[nEntries];
+     Int_t *myeventID = new Int_t[nEntries];;
+
+     //tree->SetBranchStatus("*",0); //disable all branches
+
+     tree->SetBranchAddress("posX", &posX);
+     tree->SetBranchAddress("posY", &posY);
+     tree->SetBranchAddress("posZ", &posZ);
+     tree->SetBranchAddress("photonID", &photonID);
+     tree->SetBranchAddress("eventID", &eventID);
+
+     for (Long64_t iEntry = 0; iEntry < nEntries; ++iEntry) {
+         // Load the data for the given tree entry
+         tree->GetEntry(iEntry);
+
+         // Transfer the current leaf to my arrays
+         myposX[iEntry] = posX;
+         myposY[iEntry] = posY;
+         myposZ[iEntry] = posZ;
+         myphotonID[iEntry] = photonID;
+         myeventID[iEntry] = eventID;
+
+         // Controllo se funziona, e funziona
+         if (iEntry < 50)
+         {
+             std::cout << photonID << " = " << myphotonID[iEntry] << ", " << eventID << " = "<< myeventID[iEntry] << ", " << posX << " = " << myposX[iEntry] << std::endl;
+         }
+     }
+
+     Long64_t i = 0;
+     Float_t dx1, dy1, dz1, dx2, dy2, dz2;
+     Float_t distance;
+
+     TH1D *hDistanceScattering = new TH1D("hDistanceScattering", "Scattering Distance;Distance [mm];Counts", 1000, 0, 30);
+
+     std::cout << "Scattering events: \n" << std::endl;
+
+     while(i < nEntries) {
+         // Retrieve eventID entries to check the same-event-photons
+         if( myeventID[i] == myeventID[i+1] && myeventID[i+1] == myeventID[i+2] )
+         {
+             // This event has a scattering event!
+             // have to check which couple (i, i+1 or i+1, i+2) is actually a scattering
+
+             // First couple
+             dx1 = TMath::Abs( myposX[i] - myposX[i+1]);
+             dy1 = TMath::Abs( myposY[i] - myposY[i+1]);
+             dz1 = TMath::Abs( myposZ[i] - myposZ[i+1]);
+
+             // Second couple
+             dx2 = TMath::Abs( myposX[i+1] - myposX[i+2]);
+             dy2 = TMath::Abs( myposY[i+1] - myposY[i+2]);
+             dz2 = TMath::Abs( myposZ[i+1] - myposZ[i+2]);
+
+             // First couple condition
+             if(dx1 < 30)
+             {
+                 distance = TMath::Sqrt(dx1 * dx1 + dy1 * dy1 + dz1 * dz1);
+
+                 // Display some outputs...
+                 std::cout << myphotonID[i] << ", " << i << ", "<< myeventID[i] << ", (" << dx1 << "," << dy1 << "," << dz1 << ")"<< std::endl;
+
+                 hDistanceScattering->Fill(distance);
+             }
+             // Second couple condition
+             else
+             {
+                 distance = TMath::Sqrt(dx1 * dx1 + dy1 * dy1 + dz1 * dz1);
+
+                 // Display some outputs...
+                 std::cout << myphotonID[i] << ", " << i << ", "<< myeventID[i] << ", (" << dx1 << "," << dy1 << "," << dz1 << ")"<< std::endl;
+                 hDistanceScattering->Fill(distance);
+             }
+             // Now we skip to the following 2 events (already processed)
+             i = i+3;
+         }
+         else
+         {
+             i++; // Scattering not found, we keep scrolling each eventID
+         }
+     }
+
+     TCanvas c1;
+     hDistanceScattering->Draw();
+     c1.SaveAs("distanceScatteringPhotons.root");
+
+     /*delete [] eventID;
+      *    delete [] posX;
+      *    delete [] posY;
+      *    delete [] posZ;
+      *    delete [] photonID;*/
+     //delete hDistanceScattering;
 
 
-    std::unique_ptr<TFile> myFile( TFile::Open("filtered_file_photon1.root") );
-    auto tree = myFile->Get<TTree>("filteredHits");
-
-    //std::vector<Float_t> posX, posY, posZ; //non posso dichiararli vector perché SetBranchAddress vuole il tipo esatto di variabile (non vector)
-    //Float_t posX[10], posY[10], posZ[10];
-    Float_t *posX, *posY, *posZ;
-    Int_t *photonID, *eventID;
-    Int_t *row;
-    tree->SetBranchAddress("posX", &posX);
-    tree->SetBranchAddress("posY", &posY);
-    tree->SetBranchAddress("posZ", &posZ);
-    tree->SetBranchAddress("photonID", &photonID);
-    tree->SetBranchAddress("eventID", &eventID);
-
-    for (Long64_t iEntry = 0; tree->LoadTree(iEntry) >= 0; ++iEntry) {
-        // Load the data for the given tree entry
-        tree->GetEntry(iEntry);
-        row[iEntry] = iEntry;
-    }
-
-    Long64_t i = 0;
-    Float_t dx1, dy1, dz1, dx2, dy2, dz2;
-    Float_t distance;
-
-    TH1D *hDistanceScattering = new TH1D("hDistanceScattering", "Scattering Distance;Distance [units]];Counts", 100, 0, 50);
-
-    while(i < sizeof(row)) {
-        // Retrieve eventID entries to check the same-event-photons
-        if(eventID[i] == eventID[i+1] && eventID[i+1] == eventID[i+2])
-        {
-            // This event has a scattering event!
-            // have to check which couple (i, i+1 or i+1, i+2) is actually a scattering
-
-            // First couple
-            dx1 = TMath::Abs(posX[i] - posX[i+1]);
-            dy1 = TMath::Abs(posY[i] - posY[i+1]);
-            dz1 = TMath::Abs(posZ[i] - posZ[i+1]);
-
-            // Second couple
-            dx2 = TMath::Abs(posX[i+1] - posX[i+2]);
-            dy2 = TMath::Abs(posY[i+1] - posY[i+2]);
-            dz2 = TMath::Abs(posZ[i+1] - posZ[i+2]);
-
-            // First couple condition
-            if(dx1 < 30)
-            {
-                distance = TMath::Sqrt(dx1 * dx1 + dy1 * dy1 + dz1 * dz1);
-                hDistanceScattering->Fill(distance);
-            }
-            // Second couple condition
-            else
-            {
-                distance = TMath::Sqrt(dx1 * dx1 + dy1 * dy1 + dz1 * dz1);
-                hDistanceScattering->Fill(distance);
-            }
-            // Now we skip to the following two events (already processed)
-            i = i+3;
-        }
-        else
-        {
-            i++; // Scattering not found, we keep scrolling each eventID
-        }
-    }
-
-    TCanvas c1;
-    hDistanceScattering->Draw();
-    c1.SaveAs("distanceScatteringPhoton1.root");
-
-    delete [] eventID;
-    delete [] posX;
-    delete [] posY;
-    delete [] posZ;
-    delete [] photonID;
-    delete hDistanceScattering;
-
-
-    return 0;
+     return 0;
 }
 
 
